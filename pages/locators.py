86098158ab21fa943bd936360e9f414f41fc8cce
Max <maxdv-93@mail.ru>
2021-06-11 00:10:03 +0300

    add class loginpage

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTRATION_FORM = (By.ID, "registration_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+link = "http://selenium1py.pythonanywhere.com/en-gb/"
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +14,4 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
